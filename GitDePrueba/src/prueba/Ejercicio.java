7eeab352f81d98fcdf4da6e8b3590640fa6e0392
Pedro <pedroRHGA@gmail.com>
2024-04-30 13:01:49 +0200

    Tercer Commit con un syso("Buenos Dias")

--- GitDePrueba/src/prueba/Ejercicio.java
+++ GitDePrueba/src/prueba/Ejercicio.java
@@ -5,5 +5,6 @@ public class Ejercicio {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo!");
 		System.out.println("Adios mundo!");
+		System.out.println("Buenos dias!");
 	}
 }
